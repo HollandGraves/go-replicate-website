0590306292140f53f15ce0d571039a50383d00ab
Holland Graves <zacharyledoux13@gmail.com>
2018-10-27 17:22:56 -0600

    fixed a few errors in syntax

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ type Page struct {
 func main() {
 	http.HandleFunc("/view/", viewHandler)
 	http.HandleFunc("/edit/", editHandler)
-	http.HandleFunc("/save/", saveHandler)
+	// http.HandleFunc("/save/", saveHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
@@ -60,8 +60,8 @@ func editHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		p = &Page{Title: title}
 	}
-	fmt.Fprintf(w, "<h1>Editing &s</h1>"+
-		"<form action=\"/save/&s\" method=\"POST\">"+
+	fmt.Fprintf(w, "<h1>Editing %s</h1>"+
+		"<form action=\"/save/%s\" method=\"POST\">"+
 		"<textarea name=\"body\">%s</textarea><br>"+
 		"<input type=\"submit\" value=\"Save\">"+
 		"</form>",
